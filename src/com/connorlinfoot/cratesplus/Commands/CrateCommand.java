71baa41facfff4a6195271a5e564c2a1e8efa45b
Connor Linfoot <connorlinfoot@me.com>
2015-03-17 13:36:09 +0000

    Work on /crate command

--- src/com/connorlinfoot/cratesplus/Commands/CrateCommand.java
+++ src/com/connorlinfoot/cratesplus/Commands/CrateCommand.java
@@ -1,7 +1,9 @@
 package com.connorlinfoot.cratesplus.Commands;
 
 import com.connorlinfoot.cratesplus.CrateHandler;
+import com.connorlinfoot.cratesplus.CrateType;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,12 +18,71 @@ public class CrateCommand implements CommandExecutor {
             return false;
         }
 
-        if (args.length == 1) {
-            Player player = Bukkit.getPlayer(args[0]);
-            if (player == null) return false;
+//        if (args.length == 1) {
+//            Player player = Bukkit.getPlayer(args[0]);
+//            if (player == null) return false;
+//
+//            CrateHandler.giveCrateKey(player);
+//        }
+
+        if (args.length == 0 || (args.length >= 1 && args[0].equalsIgnoreCase("help"))) {
+            // Help messages
+            return true;
+        }
+
+        if (args.length >= 1 && args[0].equalsIgnoreCase("key")) {
+            if (args.length != 2) {
+                return false;
+            }
+
+            Player player = Bukkit.getPlayer(args[1]);
+            if (player == null) {
+                sender.sendMessage(ChatColor.RED + "The player " + args[1] + " was not found");
+                return false;
+            }
 
             CrateHandler.giveCrateKey(player);
+            sender.sendMessage(ChatColor.GREEN + "Given " + player.getDisplayName() + ChatColor.RESET + ChatColor.GREEN + " a crate key");
+            return true;
         }
+
+        if (args.length >= 1 && args[0].equalsIgnoreCase("crate")) {
+            Player player;
+            CrateType crateType;
+            if (args.length == 1) {
+                sender.sendMessage(ChatColor.RED + "Correct Usage: /crate crate <type> [player]");
+                return false;
+            }
+
+            if (args.length == 3) {
+                player = Bukkit.getPlayer(args[2]);
+            } else if (sender instanceof Player) {
+                player = (Player) sender;
+            } else {
+                sender.sendMessage(ChatColor.RED + "Correct Usage: /crate crate <type> [player]");
+                return false;
+            }
+
+            if (player == null) {
+                sender.sendMessage(ChatColor.RED + "The player " + args[1] + " was not found");
+                return false;
+            }
+
+            try {
+                crateType = CrateType.valueOf(args[1].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                sender.sendMessage(ChatColor.RED + "Please specify a valid crate type");
+                return false;
+            }
+
+            CrateHandler.giveCrate(player, crateType);
+            sender.sendMessage(ChatColor.GREEN + "Given " + player.getDisplayName() + ChatColor.RESET + ChatColor.GREEN + " a crate");
+            return true;
+        }
+
+
+
+
         return true;
     }
 
