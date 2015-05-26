7a2f47c31ba4536ffa8ccffefb7d98b90b738a2d
Connor Linfoot <connorlinfoot@me.com>
2015-05-26 15:33:00 +0100

    Added /crate reload to help menu

--- src/main/java/com/connorlinfoot/cratesplus/Commands/CrateCommand.java
+++ src/main/java/com/connorlinfoot/cratesplus/Commands/CrateCommand.java
@@ -168,6 +168,7 @@ public class CrateCommand implements CommandExecutor {
 
         // Help Messages
         sender.sendMessage(CratesPlus.pluginPrefix + ChatColor.AQUA + "----- CratePlus Help -----");
+        sender.sendMessage(CratesPlus.pluginPrefix + ChatColor.AQUA + "/crate reload - Reload configuration for CratesPlus (Experimental)");
         sender.sendMessage(CratesPlus.pluginPrefix + ChatColor.AQUA + "/crate key <player> [type] - Give player a random crate key");
         sender.sendMessage(CratesPlus.pluginPrefix + ChatColor.AQUA + "/crate crate <type> [player] - Give player a crate to be placed");
 
